60c1d709dad62ec6f0600bcdb44d63b629e8aaa4
Documenter.jl <documenter@juliadocs.github.io>
2021-11-21 14:14:22 +0000

    build based on 38aa4ef

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BallroomSkatingSystem","category":"page"},{"location":"#BallroomSkatingSystem","page":"Home","title":"BallroomSkatingSystem","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BallroomSkatingSystem.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BallroomSkatingSystem]","category":"page"}]
+}
